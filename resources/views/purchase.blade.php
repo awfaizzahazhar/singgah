c92e891663e4fa427f01ed9d5eee11f5dd5841b0
awfaizzahazhar <72841382+awfaizzahazhar@users.noreply.github.com>
2022-01-02 00:26:27 +0800

    Update purchase.blade.php

--- resources/views/purchase.blade.php
+++ resources/views/purchase.blade.php
@@ -59,14 +59,46 @@ tr:nth-child(even) {
                 	<th> Image </th>
                 </tr>
                 	<td><img src="cover/{{ $data->cover }}" class="img-responsive" style="max-height:100px; max-width:100px" alt="" srcset=""></td> -->
-
+                	
                 	<tr>
                 	<th> How many will you be buying? </th>
-                </tr>
+                    </tr>
                 	<td><label for="quantity"></label>
-  					<input type="number" id="quantity" name="quantity" min="1" max="{{$data['stock']}}" >&nbsp;<a href="" class="btn btn-outline-primary">Submit</a> </td>
+  					<input type="number" id="quantity" name="quantity" min="1" max="{{$data['stock']}}">&nbsp;</td>
+                    <tr>
+                    <th> Total: RM 
+                    <span class="amount-total"></span></th>
 
-  
+                  
+
+                    </tr>
+                </table>
+
+               <b>Choose your payment method</b>
+                <br>
+                <br>
+
+                <input type="radio" name="result" value="maybank2u" checked> Online Banking   
+
+                <img src="https://www.nicepng.com/png/detail/123-1232443_click-vector-digital-online-banking-transparent.png" style="max-height:100px; max-width:100px" >
+                <br>
+                <br>
+
+                 <input type="radio" name="result" value="credit"> Credit Card
+
+                  <img src="https://thumbs.dreamstime.com/b/delhi-india-february-popular-credit-card-companies-logos-including-mastercard-visa-american-express-more-211749084.jpg" style="max-height:200px; max-width:200px" >
+
+                 <br>
+
+                
+
+
+
+
+                    &nbsp;&nbsp;
+                    <a href="/complete" class="btn btn-outline-primary">Complete Payment</a>
+
+                    </div>  
 
 
 
@@ -126,3 +158,54 @@ tr:nth-child(even) {
     </div>
 
 </x-app-layout>
+
+
+
+<script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+
+
+<script type="text/javascript">
+
+
+
+$(document).ready(function(argument) {
+
+// 	if (person == null || person == "") {
+//   text = "User cancelled the prompt.";
+// } else {
+//   text = "Hello " + person + "! How are you today?";
+// }
+
+
+
+    var price = parseFloat("{{$data['price']}}");
+    var stock = parseInt("{{$data['stock']}}");
+
+
+
+    $('#quantity').keyup(function(){
+        quantity = parseInt($(this).val());
+
+        $('.amount-total').html(quantity*price);
+       
+    });
+    $('#quantity').change(function(){
+        quantity = parseInt($(this).val());
+        $('.amount-total').html(quantity*price);
+    })
+
+    
+     // $('.quantity-max').html(
+     //    	if(quantity>stock)
+     //    	{
+     //    		window.alert("sometext");
+     //    	};
+        
+
+
+
+
+
+
+})    
+</script>
