0b3bf738f34014b08800f5f40d06f6df678b13aa
awfaizzahazhar <72841382+awfaizzahazhar@users.noreply.github.com>
2022-01-02 00:26:51 +0800

    Add files via upload

---
+++ resources/views/complete.blade.php
@@ -0,0 +1,37 @@
+<x-app-layout>
+    <x-slot name="header">
+   
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Order Confirmed') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <br>
+                <h3> <center> Your Order Has Been Confirmed! <center> </h3>
+                    <br>
+
+             <center><h2><b>Your ORDER ID is</h2></b>
+              (Please show this number to the seller upon picking up)
+                <img src="https://c.tenor.com/0AVbKGY_MxMAAAAM/check-mark-verified.gif"></center>
+
+               <center> You can view your purchase history<a href="/history"> <u>here</a> </u></center>
+            </div>
+        </div>
+    </div>
+
+     <!-- Font-awesome -->
+    
+                  
+              
+                        </form>
+                       </td>
+                </tr>
+            
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
