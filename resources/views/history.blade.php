fdf8b7aff25138b74781740c6dd086436d3f24b0
awfaizzahazhar <72841382+awfaizzahazhar@users.noreply.github.com>
2022-01-02 00:25:17 +0800

    Add files via upload

---
+++ resources/views/history.blade.php
@@ -0,0 +1,77 @@
+<x-app-layout>
+    <x-slot name="header">
+   
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Purchase History and Order History') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <br>
+                <h3> <center> Your Purchase History <center> </h3>
+            
+               <center> See your past purchase history here <center>
+                <center>
+
+                    <table>
+                    <thead>
+                        <tr>
+                            <th>Order ID &nbsp;&nbsp; </th>
+                            <th> &nbsp;Food Name &nbsp;&nbsp; </th>
+                            <th> &nbsp;Location and Date&nbsp;&nbsp;  </th>
+                            <th> &nbsp;Price&nbsp;&nbsp;  </th>
+                             <th> &nbsp;Quantity&nbsp;&nbsp;  </th>
+                             
+                            
+                           
+                        </tr>
+                    </thead>
+                </tbody>
+                
+                 <tr>
+                   <td>1</td>
+                   <td>2</td>
+                    <td>3 </td>
+                    <td>4</td>
+                    <td>5</td>
+                </tr>
+
+                    
+
+                </center></table>
+            </div>
+        </div>
+    </div>
+ <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <br>
+               
+               <h3> <center> Your Order History</center> </h3>
+                <center>See the people who purchased your food!</center>
+                <br>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+     <!-- Font-awesome -->
+    
+                  
+              
+                        </form>
+                       </td>
+                </tr>
+            
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
